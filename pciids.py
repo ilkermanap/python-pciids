835a954de2cfa9865a75827ab4e6f5f6ceb8591f
ilkermanap <ilkermanap@gmail.com>
2015-01-30 01:03:52 +0100

    added documentation to file

--- pciids.py
+++ pciids.py
@@ -7,12 +7,26 @@ global HOME
 HOME = "https://pci-ids.ucw.cz"
 
 class Vendor:
+    """
+    Class for vendors. This is the top level class
+    for the devices belong to a specific vendor.
+    self.devices is the device dictionary
+    subdevices are in each device.
+    """
     def __init__(self, vendorStr):
+        """
+        Class initializes with the raw line from pci.ids
+        Parsing takes place inside __init__
+        """
         self.ID = vendorStr.split()[0]
         self.name = vendorStr.replace("%s " % self.ID,"")
         self.devices = {}
 
     def addDevice(self, deviceStr):
+        """
+        Adds a device to self.devices
+        takes the raw line from pci.ids
+        """
         s = deviceStr.strip()
         devID = s.split()[0]
         if devID in self.devices:
@@ -27,6 +41,10 @@ class Vendor:
 
 class Device:
     def __init__(self, deviceStr):
+        """
+        Class for each device.
+        Each vendor has its own devices dictionary.
+        """
         s = deviceStr.strip()
         self.ID = s.split()[0]
         self.name = s.replace("%s  " % self.ID,"")
@@ -38,6 +56,10 @@ class Device:
             subdev.report()
 
     def addSubDevice(self, subDeviceStr):
+        """
+        Adds a subvendor, subdevice to device.
+        Uses raw line from pci.ids
+        """
         s = subDeviceStr.strip()
         spl = s.split()
         subVendorID  = spl[0]
@@ -47,7 +69,13 @@ class Device:
         self.subdevices[devID] = SubDevice(subVendorID,subDeviceID,subDeviceName)
 
 class SubDevice:
+    """
+    Class for subdevices.
+    """
     def __init__(self, vendor, device, name):
+        """
+        Class initializes with vendorid, deviceid and name
+        """
         self.vendorID = vendor
         self.deviceID = device
         self.name = name
@@ -56,7 +84,17 @@ class SubDevice:
         print "\t\t%s\t%s\t%s" % (self.vendorID, self.deviceID,self.name)
 
 class PCIIds:
+    """
+    Top class for all pci.ids entries.
+    All queries will be asked to this class.
+    PCIIds.vendors["0e11"].devices["0046"].subdevices["0e11:4091"].name  =  "Smart Array 6i" 
+    """
     def __init__(self):
+        """
+        Prepares the directories. 
+        Checks local data file. 
+        Tries to load from local, if not found, downloads from web
+        """
         self.version = ""
         self.compressed = "pci.ids.bz2"
         subprocess.call(['mkdir -p data'], shell=True)
@@ -66,10 +104,17 @@ class PCIIds:
         self.parse()
 
     def reportVendors(self):
+        """Reports the vendors
+        """
         for vid, v in self.vendors.items():
             print v.ID, v.name
 
     def report(self, vendor = None):
+        """
+        Reports everything for all vendors or a specific vendor
+        PCIIds.report()  reports everything
+        PCIIDs.report("0e11") reports only "Compaq Computer Corporation"
+        """
         if vendor != None:
             self.vendors[vendor].report()
         else:
@@ -99,6 +144,9 @@ class PCIIds:
                         self.vendors[vendorID] = Vendor(l)
 
     def getLatest(self):
+        """
+        
+        """
         ver, url = self.latestVersion()
         outfile = "data/%s-%s" % (ver, self.compressed)
         out = open(outfile, "w")
@@ -109,9 +157,16 @@ class PCIIds:
         self.readLocal()
 
     def readLocal(self):
+        """
+        Reads the local file
+        """
         self.contents = open("data/%s-pci.ids" % self.version).readlines()
 
     def loadLocal(self):
+        """
+        Loads database from local. If there is no file, 
+        it creates a new one from web
+        """
         idsfile = glob.glob("data/*.ids")
         if len(idsfile) == 0:
             self.getLatest()
@@ -120,6 +175,9 @@ class PCIIds:
             self.readLocal()
 
     def latestVersion(self):
+        """
+        Checks the latest version from web
+        """
         webPage = urllib2.urlopen(HOME).readlines()
         for line in webPage:
              if line.find(self.compressed) > -1:
